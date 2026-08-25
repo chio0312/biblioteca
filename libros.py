6fcb3441777b55e6d6570c50c7af74a23386396f
chio0312 <erociofigs@gmail.com>
2026-08-25 07:50:33 -0600

    cambio corregido

--- libros.py
+++ libros.py
@@ -4,7 +4,7 @@ LIBROS = [
         "titulo": "Lazarillo de Tormes",
         "autor": "Anónimo",
         "categoria": "Novelas y narrativa",
-        "ejemplares": 6
+        "ejemplares": 7
     },
 
     {
@@ -18,7 +18,7 @@ LIBROS = [
         "titulo": "Don Quijote de la Mancha",
         "autor": "Miguel de Cervantes",
         "categoria": "Novelas y narrativa",
-        "ejemplares": 6
+        "ejemplares": 7
     },
 
     {
@@ -39,21 +39,21 @@ LIBROS = [
         "titulo": "La mansión del pájaro serpiente",
         "autor": "Virgilio Rodríguez Macal",
         "categoria": "Novelas y narrativa",
-        "ejemplares": 2
+        "ejemplares": 3
     },
 
     {
         "titulo": "El Principito",
         "autor": "Antoine de Saint-Exupéry",
         "categoria": "Novelas y narrativa",
-        "ejemplares": 2
+        "ejemplares": 4
     },
 
     {
         "titulo": "El hombre que lo tenía todo, todo, todo",
         "autor": "Miguel Ángel Asturias",
         "categoria": "Novelas y narrativa",
-        "ejemplares": 1
+        "ejemplares": 2
     },
 
     {
@@ -84,6 +84,131 @@ LIBROS = [
         "ejemplares": 1
     },
 
+    {
+        "titulo": "Marianela",
+        "autor": "Benito Perez",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 2
+    },
+
+    {
+        "titulo": "El canasto de sastre",
+        "autor": "Jose Milla",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 2
+    },
+
+    {
+        "titulo": "Carazamba",
+        "autor": "Vrigilio Rodriguez",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El misterio de agua escondida",
+        "autor": "Luis Felipe Ortiz",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Dos años de vacaciones",
+        "autor": "Julio Verne",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El mundo del misterio verde",
+        "autor": "Virgilio Rodriguez Macal",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Tulinet, las siete vidas del gato",
+        "autor": "Ricardo Alcantara",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Hace falta un muchacho",
+        "autor": "Luis Gonzales Obregon",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Memorias de un abogado",
+        "autor": "Jose Milla",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El tunes",
+        "autor": "Ernesto Sabato",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 3
+    },
+
+    {
+        "titulo": "Historia de un pepe",
+        "autor": "Jose Milla",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El laboratorio secreto",
+        "autor": "Lluis Prats",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Huasipungo",
+        "autor": "Jorge Icaza",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Sangre de campeón",
+        "autor": "Carlos Cuauhtémoc",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Los otros son horribles",
+        "autor": "J.L. Perdomo Orellana",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Don Segunda Sombra",
+        "autor": "R. Guirales",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Todos los osos son zurdos",
+        "autor": "Ignacio Padilla",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "La llave de un gran árbol",
+        "autor": "Juan Manuel Salazar",
+        "categoria": "Novelas y narrativas",
+        "ejemplares": 1
+    },
 
     # POESÍA
 
@@ -91,7 +216,7 @@ LIBROS = [
         "titulo": "Rubén Darío: Poemas",
         "autor": "Rubén Darío",
         "categoria": "Poesía",
-        "ejemplares": 3
+        "ejemplares": 6
     },
 
     {
@@ -112,7 +237,7 @@ LIBROS = [
         "titulo": "Rafael Landívar: Poesía",
         "autor": "Rafael Landívar",
         "categoria": "Poesía",
-        "ejemplares": 4
+        "ejemplares": 5
     },
 
     {
@@ -122,6 +247,26 @@ LIBROS = [
         "ejemplares": 2
     },
 
+    {
+        "titulo": "Rimas y leyendas",
+        "autor": "Gustavo Bécquer",
+        "categoria": "Poesía",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Poemas Espiral",
+        "autor": "Carlos Palma",
+        "categoria": "Poesía",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Azul",
+        "autor": "Rúben Dario",
+        "categoria": "Poesía",
+        "ejemplares": 1
+    },
 
     # TEATRO
 
@@ -129,16 +274,15 @@ LIBROS = [
         "titulo": "Los árboles mueren de pie",
         "autor": "Alejandro Casona",
         "categoria": "Teatro / obras dramáticas",
-        "ejemplares": 4
+        "ejemplares": 6
     },
 
     {
         "titulo": "La vida es un sueño",
         "autor": "Pedro Calderón de la Barca",
         "categoria": "Teatro / obras dramáticas",
-        "ejemplares": 1
+        "ejemplares": 2
     },
-
 
     # OBRAS RELIGIOSAS
 
@@ -146,7 +290,7 @@ LIBROS = [
         "titulo": "Popol Vuh",
         "autor": "Anónimo",
         "categoria": "Obras religiosas y tradicionales",
-        "ejemplares": 6
+        "ejemplares": 9
     },
 
     {
@@ -156,6 +300,12 @@ LIBROS = [
         "ejemplares": 2
     },
 
+    {
+        "titulo": "Rompiendo las cadenas",
+        "autor": "Neil Anderson",
+        "categoria": "Obras religiosas y tradicionales",
+        "ejemplares": 1
+    },
 
     # HISTORIA
 
@@ -166,6 +316,47 @@ LIBROS = [
         "ejemplares": 2
     },
 
+    {
+        "titulo": "La hija del adelantado",
+        "autor": "Jose Milla",
+        "categoria": "Historia / autobiografia / testimonio",
+        "ejemplares": 2
+    },
+
+    {
+        "titulo": "Barrios, Dictador",
+        "autor": "Jose Santacruz",
+        "categoria": "Historia / autobiografia / testimonio",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "America en la profecia",
+        "autor": "E. G. White",
+        "categoria": "Historia / autobiografia / testimonio",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El señor presidente",
+        "autor": "Miguel Angel Asturias",
+        "categoria": "Historia / autobiografia / testimonio",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El Libertador",
+        "autor": "Horacio Marroquin",
+        "categoria": "Historia / autobiografia / testimonio",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "La bibliografia guatemalteca",
+        "autor": "Horacio Marroquin",
+        "categoria": "Historia / autobiografia / testimonio",
+        "ejemplares": 1
+    },
 
     # DERECHO
 
@@ -173,7 +364,164 @@ LIBROS = [
         "titulo": "Constituciones políticas",
         "autor": "Varios",
         "categoria": "Derecho / documentos políticos",
+        "ejemplares": 4
+    },
+
+    {
+        "titulo": "Código de trabajo",
+        "autor": "Varios",
+        "categoria": "Derecho / documentos políticos",
+        "ejemplares": 1
+    },
+
+    # CUENTOS Y LEYENDAS
+
+    {
+        "titulo": "Cuentos y leyendas de Guatemala",
+        "autor": "Francisco Barnoya",
+        "categoria": "Cuentos y leyendas",
         "ejemplares": 2
+    },
+
+    {
+        "titulo": "Guardasecretos y otros cuentos",
+        "autor": "Diana Garciadueñas",
+        "categoria": "Cuentos y leyendas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Cuentos de barro",
+        "autor": "Salarrué",
+        "categoria": "Cuentos y leyendas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Leyendas de Guatemala",
+        "autor": "Miguel Angel Asturias",
+        "categoria": "Cuentos y Leyendas",
+        "ejemplares": 1
+    },
+
+    # BIOGRAFÍAS
+
+    {
+        "titulo": "1000 Biografias Abreviadas",
+        "autor": "Juan Regla Campistol",
+        "categoria": "Biografias",
+        "ejemplares": 1
+    },
+
+    # MOTIVACIÓN
+
+    {
+        "titulo": "Date un chance",
+        "autor": "Clemente de Leon",
+        "categoria": "Motivacionales",
+        "ejemplares": 4
+    },
+
+    {
+        "titulo": "Historias de la vida real",
+        "autor": "Varios",
+        "categoria": "Motivacionales",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El rinoceronte",
+        "autor": "Scott Alexander",
+        "categoria": "Motivacionales",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Los 7 hábotos de la gente altamente nociva",
+        "autor": "Rodolfo Munguía",
+        "categoria": "Motivacionales",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "La vaca",
+        "autor": "Miguel Cruz",
+        "categoria": "Motivacionales",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El exito no llega por casualdad",
+        "autor": "Dr. Lair Ribeiro",
+        "categoria": "Motivacionales",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El rinoceronte I",
+        "autor": "Scott Alexander",
+        "categoria": "Motivacionales",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El rinoceronte II",
+        "autor": "Scott Alexander",
+        "categoria": "Motivacionales",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "El rinoceronte III",
+        "autor": "Scott Alexander",
+        "categoria": "Motivacionales",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Quién se ha llevado mi queso?",
+        "autor": "Spencer Johnson",
+        "categoria": "Motivacionales",
+        "ejemplares": 1
+    },
+
+    # IDIOMAS Y DICCIONARIOS
+
+    {
+        "titulo": "Family English",
+        "autor": "DIDACO S.A.",
+        "categoria": "Idiomas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Family English 2",
+        "autor": "DIDACO S.A.",
+        "categoria": "Idiomas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Diccionario ESPAÑOL-INGLES, INGLÉS-ESPAÑOL",
+        "autor": "Desconocido",
+        "categoria": "Idiomas",
+        "ejemplares": 1
+    },
+
+    {
+        "titulo": "Basic Commercial English",
+        "autor": "Berta Avila",
+        "categoria": "Idiomas",
+        "ejemplares": 1
+    },
+
+    # GEOGRAFÍA
+
+    {
+        "titulo": "Guía Visualizada CENTROAMÉRICA",
+        "autor": "Julio Piedrasanta",
+        "categoria": "Geografía",
+        "ejemplares": 1
     }
 
 ]
